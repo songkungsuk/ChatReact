ecaa8e743c5bdabefeec2eb75aeaad8e0efe3b43
cust <cust02@naver.com>
2024-01-22 18:09:02 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import { Main } from './components/Main';
+import { Login } from './components/Login';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { SignUp } from './components/SignUp';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+      <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={'/'}>
+                Chatting
+              </Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-in'}>
+                      SignIn
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-up'}>
+                      SignUp
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div className="auth-wrapper">
+            
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/sign-in" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+              </Routes>
+            
+          </div>
+        </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react'
+import { ChatUserInfo } from '../types/ChatUserInfo.type';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const Login = () => {
+  const [rememberId, setRememberId] = useState(false);
+  const [chatUser, setChatUser] = useState<ChatUserInfo>({});
+  const [error, setError] = useState<boolean>(false);
+  const navigate = useNavigate();
+  let chiId: any = localStorage.getItem('chiId');
+  useEffect(() => {
+    let chiId: any = localStorage.getItem('chiId');
+    if (chiId) {
+      setChatUser({
+        ...chatUser,
+        chiId: chiId
+      })
+      setRememberId(true);
+    }
+  }, [])
+
+
+
+  const checkRememberId = (evt: any) => {
+    setRememberId(evt.target.checked);
+  }
+  const changeUser = (event: any) => {
+
+    setChatUser({
+      ...chatUser,
+      [event.target.id]: event.target.value
+    })
+    console.log(chatUser);
+  }
+  const login = async () => {
+    const res = await axios.post('http://localhost/login', chatUser, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    }).then(res => {
+      alert('로그인이 완료되었습니다.');
+      localStorage.setItem('user', JSON.stringify(res.data));
+      if (rememberId) {
+        localStorage.setItem('chiId', res.data.chiId);
+      } else {
+        localStorage.removeItem('chiId');
+      }
+      navigate('/');
+    })
+      .catch(err => {
+        setError(true);
+        console.log(err);
+      })
+    console.log(res);
+  }
+
+  return (
+    <div className="auth-inner">
+      <form>
+        <h3>Sign In</h3>
+
+        <div className="mb-3">
+          {error
+            ?
+            <div className='text-danger'>
+              아이디와 비밀번호를 확인해주세요.
+            </div> : ''
+          }
+          <label>아이디</label>
+          <input type="text" className="form-control" placeholder="아이디" onChange={changeUser} id='chiId' value={chiId} />
+        </div>
+
+        <div className="mb-3">
+          <label>비밀번호</label>
+          <input type="password" className="form-control" placeholder="비밀번호" onChange={changeUser} id='chiPwd' />
+        </div>
+
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="customCheck1" onChange={checkRememberId} checked={rememberId} />
+            <label className="custom-control-label" htmlFor="customCheck1" >
+              아이디 저장
+            </label>
+          </div>
+        </div>
+
+        <div className="d-grid">
+          <button type="button" className="btn btn-primary" onClick={login}>
+            Sign In
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          <a href="#">회원가입</a>
+        </p>
+      </form>
+    </div>
+  )
+}
